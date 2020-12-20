9740b02a3e07103ca619b0506231d644870b9909
biswa7064 <biswaranjan16856@gmail.com>
2020-12-20 22:44:50 +0530

    initialized firebase database,storage and config firebase with our project

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+
+
+const config = {
+    apiKey: "AIzaSyArXuuKUMPZnAj-e8eLfbIKYmLhYH_nYQ0",
+    authDomain: "chatapp-f2912.firebaseapp.com",
+    databaseURL: "https://chatapp-f2912-default-rtdb.firebaseio.com",
+    projectId: "chatapp-f2912",
+    storageBucket: "chatapp-f2912.appspot.com",
+    messagingSenderId: "49066657053",
+    appId: "1:49066657053:web:86a0f2c1a4b3b4115d0df4"
+  };
+
+  const app = firebase.initializeApp(config);
