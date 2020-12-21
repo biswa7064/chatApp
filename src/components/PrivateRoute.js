eb9ce611872808f83f8705a18256ae91628b87c3
biswa7064 <biswaranjan16856@gmail.com>
2020-12-21 23:24:59 +0530

    global state management[ adding spinner and check for subscribed and unsubscribed dunsubscribed user using userref.on and .off]

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,15 +1,30 @@
 /* eslint-disable arrow-body-style */
 import React from 'react'
 import { Redirect, Route } from 'react-router';
+import { Container, Loader } from 'rsuite';
 import { useProfile } from '../context/profile.context';
 
 
 
 
-const PrivateRoute = ({ children, ...routeProps}) => {
-    const profile = useProfile();
 
-    if(!profile){
+const PrivateRoute = ({ children, ...routeProps}) => {
+    const { profile, isLoading } = useProfile();
+
+// if isloading and no profile then loading occured
+    if(isLoading && !profile){
+        return <Container>
+            <Loader 
+            center 
+            vertical 
+            size = "md" 
+            content = "Loading"
+             speed = "slow" />
+        </Container>
+    }
+
+    // if we have no profile and no loading process then redirect to signin page
+    if(!profile && !isLoading){
         return <Redirect to = "/signin"/>;
     }
     return (
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,14 +1,71 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
-import React,{createContext, useState, useContext} from 'react'
+import React,{createContext, useState, useContext, useEffect} from 'react'
+import { auth, database } from '../misc/firebase'
+
+
+
+
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) =>{
 
-    const [profile] = useState(false);
+    const [profile,setProfile] = useState(null);
+    const [isLoading,setIsLoading] = useState(true)
+
+    useEffect(()=>{
+
+        let userRef;
+
+      const authUnsubscribe = auth.onAuthStateChanged(authObj=>{
+
+            if(authObj){
+// check for datbase for signedin user or not signedin user
+
+               userRef = database.ref(`/profiles/${authObj.uid}`);
+
+                        userRef.on("value",snap=>{
+                            const {name,createdAt} = snap.val();
+                            
+
+                            const data = {
+                                name,
+                                createdAt,
+                                uid:authObj.uid,
+                                email: authObj.email
+                            };
+            
+                            setProfile(data);
+                            setIsLoading(false)
+                            
+                        });               
+
+            }else{
+
+                // this when signedoff
+
+                if(userRef){
+                    userRef.off();
+                }
+
+                setProfile(null);
+                setIsLoading(false)
+            }
+        });
+
+        return () =>{
+        authUnsubscribe()
+        if(userRef){
+            userRef.off();
+        }
+        
+        }
+
+    },[])
 
     return (
-    <ProfileContext.Provider value = {profile}>
+    <ProfileContext.Provider value = {{isLoading,profile}}>
         {children}
     </ProfileContext.Provider>
     );
