176539c89c39249eae7f01cf1500cc33cc059231
biswa7064 <biswaranjan16856@gmail.com>
2020-12-26 17:59:00 +0530

    created layout form home paga and created Chat page

---
+++ src/pages/Home/Chat.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+const Chat = () => {
+    return (
+        <div>
+            hello biswa
+        </div>
+    )
+}
+
+export default Chat
