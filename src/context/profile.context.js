7d4203e59e825a1549ea63adac9c44d76d925a09
biswa7064 <biswaranjan16856@gmail.com>
2020-12-29 01:14:12 +0530

    Adding real time presence i.e. when a member is online or offline

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
 import React,{createContext, useState, useContext, useEffect} from 'react'
-import { auth, database } from '../misc/firebase'
+import firebase from 'firebase/app'
+import { auth, database } from '../misc/firebase';
+
+
+
+
+export const isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 
 
@@ -14,15 +28,18 @@ export const ProfileProvider = ({ children }) =>{
     const [profile,setProfile] = useState(null);
     const [isLoading,setIsLoading] = useState(true);
 
+    
+
     useEffect(()=>{
 
         let userRef;
+        let userStatusRef;
 
       const authUnsubscribe = auth.onAuthStateChanged(authObj=>{
 
             if(authObj){
 // check for datbase for signedin user or not signedin user
-
+                userStatusRef = database.ref(`/status/${authObj.uid}`);
                userRef = database.ref(`/profiles/${authObj.uid}`);
 
 // whenever the above database data change then the bellow callbacke fired according to the data change in the database
@@ -40,12 +57,42 @@ export const ProfileProvider = ({ children }) =>{
                             setProfile(data);
                             setIsLoading(false);
                             
-                        }); 
+                        });
+                        
+                        
+                        
+
+
+                        database.ref('.info/connected').on('value', (snapshot)=> {
+                            // If we're not currently connected, don't do anything.
+                            if (snapshot.val() === false) {
+                                return;
+                            };
+                        
+                            
+                            userStatusRef
+                            .onDisconnect()
+                            .set(isOfflineForDatabase)
+                            .then(()=> {                               
+                                userStatusRef.set(isOnlineForDatabase);
+                            });
+                        });
+
+
+
              }else{
                 // this when signedoff
                 if(userRef){
                     userRef.off();
                  }
+
+
+                 if(userStatusRef){
+                     userStatusRef.off();
+                 }
+
+                 database.ref('.info/connected').off();
+
                 setProfile(null);
                 setIsLoading(false);
             }
@@ -53,6 +100,7 @@ export const ProfileProvider = ({ children }) =>{
 
         return () =>{
         authUnsubscribe();
+        database.ref('.info/connected').off();
         if(userRef){
             userRef.off();
         }
