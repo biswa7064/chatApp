485dc17e0a28df1a35b7c44f0f4397da8bc15248
biswa7064 <biswaranjan16856@gmail.com>
2020-12-30 18:26:46 +0530

    finsihed functionality for liking messages and disliking messages and also responsive functionlaity for mobile device

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -8,16 +8,18 @@ import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 import PresenceDot from '../../rooms/PresenceDot';
 import { useCurrentRoom } from '../../../context/current.room.context';
 import { auth } from '../../../misc/firebase';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
 
 
-const MessageItem = ({message, handleAdmin}) => {
+const MessageItem = ({message, handleAdmin, handleLike}) => {
 
-    const {author, createdAt, text} = message;
+    const {author, createdAt, text, likes, likeCount} = message;
 
     const [selfRef, isHovered]  = useHover();
+
+    const isMobile = useMediaQuery(`(max-width:992px)`);
 
     const isAdmin = useCurrentRoom(v => v.isAdmin);
     const admins = useCurrentRoom(v=> v.admins);
@@ -25,6 +27,9 @@ const MessageItem = ({message, handleAdmin}) => {
     const isMsgAuthorAdmin = admins.includes(author.uid);
     const isAuthor = auth.currentUser.uid === author.uid;
     const canGrantAdmin = isAdmin && !isAuthor;
+
+    const isShownOnMobile = isMobile || isHovered;
+    const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid)
 
 
     return (
@@ -51,12 +56,12 @@ const MessageItem = ({message, handleAdmin}) => {
                 className = "font-normal text-black-45 ml-2" />
 
                 <IconBtnControl
-                {...(true?{color:'red'}:{})}
-                isVisible
+                {...(isLiked?{color:'red'}:{})}
+                isVisible ={isShownOnMobile}
                 iconName = "heart"
                 tooltip ="Like thsi message"
-                onClick = {()=>{}}
-                badgeContent = {5}/>
+                onClick = {()=>{handleLike(message.id)}}
+                badgeContent = {likeCount}/>
             </div>
             <div>
                 <span className = "word-breal-all">{text}</span>
